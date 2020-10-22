ef96665198daedb58e22d8d55c0cb5a94ff539ff
zachary balder <zmbalder@gmail.com>
2020-10-22 17:23:43 -0400

    fix proxy validation in config, reorder log statements, some more npm scripts

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const key = fs.readFileSync(path.join(__dirname, 'private', 'key.pem'))
 const server = new Server({ cert, key })
 
 server
-  .start(8888, 'localhost')
+  .start(8888, '0.0.0.0')
   .then(() => console.log('Server started!'))
   .catch(err => {
     console.error(err)
