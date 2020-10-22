3c8441d693db6386092c06833931b3605637d87d
zachary balder <zmbalder@gmail.com>
2020-10-21 23:25:23 -0400

    initial

---
+++ cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const Manager = require('./lib/manager')
+
+const dir = process.argv[2]
+
+if (!dir) {
+  console.error('Expected path to directory')
+  process.exit(1)
+}
+
+const manager = new Manager()
+
+const config = fs.readFileSync(path.join(dir, 'config.json'), 'utf8')
+const script = fs.readFileSync(path.join(dir, 'script.js'), 'utf8')
+
+const job = manager.addJob(config, script)
+
+job
+  .once('done', process.exit)
+  .scheduled || job.start()
---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const Server = require('./lib/server')
+
+const cert = fs.readFileSync(path.join(__dirname, 'private', 'cert.pem'))
+const key = fs.readFileSync(path.join(__dirname, 'private', 'key.pem'))
+
+const server = new Server({ cert, key })
+
+server
+  .start(8888, 'localhost')
+  .then(() => console.log('Server started!'))
+  .catch(err => {
+    console.error(err)
+    process.exit(1)
+  })
